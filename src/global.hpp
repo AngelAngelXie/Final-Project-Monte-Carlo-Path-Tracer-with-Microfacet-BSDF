68fa91c03b85aca7ee7a482a4b0f2021e8bdb348
XajilX <xajilx@outlook.com>
2025-05-06 01:05:06 +1000

    Initialise RNG beforehand for threads, avoiding overhead by getting random from rand dev multiple times

--- src/global.hpp
+++ src/global.hpp
@@ -2,13 +2,16 @@
 #include <Eigen/Dense>
 #include <cmath>
 #include <iostream>
+#include <omp.h>
 #include <random>
 
 #undef M_PI
 #define M_PI 3.141592653589793f
 
 extern const float EPSILON;
+const int MAX_THRD = 16;
 const float kInfinity = std::numeric_limits<float>::max();
+static std::mt19937 RNGS[MAX_THRD];
 
 inline float clamp(const float &lo, const float &hi, const float &v) {
     return std::max(lo, std::min(hi, v));
@@ -36,13 +39,18 @@ inline Eigen::Vector3f lerp(const Eigen::Vector3f &a, const Eigen::Vector3f &b,
     return a * (1 - t) + b * t;
 }
 
-inline float get_random_float() {
+inline void init_rngs(int thrd_num) {
     std::random_device dev;
-    std::mt19937 rng(dev());
+    for (int i = 0; i < thrd_num; ++i) {
+        RNGS[i] = std::mt19937(dev());
+    }
+}
+
+inline float get_random_float() {
     std::uniform_real_distribution<float> dist(
         0.f, 1.f); // distribution in range [1, 6]
 
-    return dist(rng);
+    return dist(RNGS[omp_get_thread_num()]);
 }
 
 inline void UpdateProgress(float progress) {
